7650be7596aaf151114ff07ca5aaf029caec53c5
capt-alien <eric.botcher@presidio.edu>
2019-06-14 15:05:06 -0700

    solution to 1 not workng

--- prob1.py
+++ prob1.py
@@ -2,20 +2,19 @@
 
 
 def twoSum(nums, target):
-    #Create return lists
-
-    #run double for loop to 
-
-
-
-
-
-
+    #run double for loop to find a combo of numbers to get to Target
+    for i in range(0, (len(nums)-1)):
+        for x in range((i+1), (len(nums)-1)):
+            if nums[i]+ nums[x] == target:
+                return [i, x]
 
 #test
-if __main__=='__main__':
-    List_a=[1, 3, 4, 5, 6, 8, 10]
+if __name__ == "__main__":
+    list_a=[1, 3, 4, 5, 6, 8, 10]
     target = 9
-    result = Solution.twoSum(list_a, Target)
+    result = twoSum(list_a, target)
     print(result)
-    print(result= [4, 5])
+    if result == [2, 3]:
+        print(True)
+    else:
+        print(False)
