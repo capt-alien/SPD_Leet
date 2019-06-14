8755cea2f19b586e47b1731fa9ce684c09b685bc
capt-alien <eric.botcher@presidio.edu>
2019-06-14 14:51:12 -0700

    double for looop

---
+++ prob1.py
@@ -0,0 +1,21 @@
+#! /bin/python3
+
+
+def twoSum(nums, target):
+    #Create return lists
+
+    #run double for loop to 
+
+
+
+
+
+
+
+#test
+if __main__=='__main__':
+    List_a=[1, 3, 4, 5, 6, 8, 10]
+    target = 9
+    result = Solution.twoSum(list_a, Target)
+    print(result)
+    print(result= [4, 5])
