81e1dfcde51ea73c6aeeac5b468a342582e8ffd8
capt-alien <eric.botcher@presidio.edu>
2019-06-14 15:07:40 -0700

    solution to 1 working

--- prob1.py
+++ prob1.py
@@ -4,13 +4,15 @@
 def twoSum(nums, target):
     #run double for loop to find a combo of numbers to get to Target
     for i in range(0, (len(nums)-1)):
+        print(i)
         for x in range((i+1), (len(nums)-1)):
+            print(x)
             if nums[i]+ nums[x] == target:
                 return [i, x]
 
 #test
 if __name__ == "__main__":
-    list_a=[1, 3, 4, 5, 6, 8, 10]
+    list_a=[1, 3, 4, 5, 7, 10]
     target = 9
     result = twoSum(list_a, target)
     print(result)
