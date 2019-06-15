7198c12fecadc43f09e297d8e458ac27aac31dbe
capt-alien <eric.botcher@presidio.edu>
2019-06-15 14:10:14 -0700

    final solution

--- prob2.py
+++ prob2.py
@@ -5,20 +5,20 @@
 #         return listify
 def isValid(string):
     listify = list(string)
-    stacka= []
+    stacka = []
     for char in listify:
         if char == '(' or char == '[' or char == '{':
             stacka.append(char)
         #Chek the last entry in stack to see if it matches
         if char == ')' or char == ']' or char == '}':
-            #return false if len()
-            check = listify[len(listify)-1]
-            if check == '(' and char == ')':
-                listify.pop()
-            elif check == '{' and char == '}':
-                listify.pop()
-            elif check == '[' and char == ']':
-                listify.pop()
+            #check last entry in stack
+            peak = stacka[len(stacka)-1]
+            if peak == '(' and char == ')':
+                stacka.pop()
+            elif peak == '{' and char == '}':
+                stacka.pop()
+            elif peak == '[' and char == ']':
+                stacka.pop()
             else:
                 return False
     #Return true if nothing left in stacka
@@ -28,17 +28,12 @@ def isValid(string):
         return False
 
 
-
-
-
-
-
-
 #test
 if __name__ == "__main__":
 
-    test_a = '(({[{[]}]()}{}){})'
+    test_a = '({[]})'
     test_b = '({)}[{]}'
     test_c = '{)[}(}'
-    print(test_a)
     print(isValid(test_a))
+    print(isValid(test_b))
+    print(isValid(test_c))
